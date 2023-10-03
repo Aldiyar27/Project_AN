43bbc0f46800f1ad82a1977022eaa8b27f214bc0
Ашкеев Алдияр Русланович <@lcoast.itstep>
2023-10-03 15:08:22 +0600

    feature 1.0.0

---
+++ project/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/home';
+import Profile from './pages/profile';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/home" element={<Home/>}/>
+        <Route exact path="/profile" element={<Profile/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ project/src/pages/profile.js
@@ -0,0 +1,27 @@
+import React from "react";
+import '../css/profile.css';
+import logo from '../img/logo.png';
+import '../fonts/Kanit/Kanit-Medium.ttf'
+import home_img from '../img/home.png';
+import profile_img from '../img/profile.png';
+import more_img from '../img/more.png';
+
+const Profile = () => {
+    return (
+        <div>
+            <div className="menu">
+                <div className="items">
+                    <div className="logo"><img src={logo} alt=""/></div>
+                    <ul>
+                        <a href={'/home'}><li className="home"><img src={home_img} alt=""/> Home </li></a>
+                        <a href={'/profile'}><li className="profile"> <img src={profile_img} alt=""/> Profile </li></a>
+                        <a href={'/more'}><li className="more"><img src={more_img} alt=""/> More</li></a>
+                    </ul> 
+                </div>
+                <div className="line"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
